eec4fb84a34b4e5708d4b05fd362ba0504cb6f5b
Ishrat Lallmamode <ishrat@asu.edu>
2024-10-31 16:09:22 -0700

    Final DiceGame

--- dom-example/dice-game.js
+++ dom-example/dice-game.js
@@ -1,123 +1,116 @@
-
 // focus 
 
 function inputFocus(id){
-    id.style.backgroundColor = "white" 
+    id.style.backgroundColor = "white"
     id.style.color = "black"
 }
 
-// generate 
 
-function generateUserName(id){
-    const firstName = ["Shah", "Lara", "John", "Zelda", "Isaac"]
-    const lastName = ["Rukh-Khan", "Croft", "117", "Hyrule", "Clarke"]
+// save name + with validation
+function saveName(){
+    let name = document.getElementById("username");
 
-    try{
-        let stringName = firstName[Math.floor(Math.random()*5)]+" "+lastName[Math.floor(Math.random()*5)]
-        if(stringName.includes("undefined"))
-        {
-            stringName = "Leeroy Jenkins (OG)"
-            id.value = stringName; 
-
-        }
-
-        id.value = stringName; 
-        
-    }
-
-    catch(err) {
-        let stringName = "Leeroy Jenkins (Mass Effect)"
-        console.log(error)
-    }
-
-        id.style.backgroundColor = "white" 
-        id.style.color = "black"
-
-}
-
-function saveUserName(){
-
-    let textInputVal = document.getElementById("username").value; 
-    return textInputVal;
-
-}
-
-
-function diceRoll(textField, pId, hId, endResult){
-
-    let name = saveUserName(); 
-
-    if(name == "" || name == " " || name == "Character's Name")
+    if(name.value == "" || name.value == " " || name.value == "Character's Name" || name.value == "Please provide valid name")
     {
-        textField.style.backgroundColor = "white"; 
-        textField.style.border = "2px solid red"
-        textField.style.color = "red"
-
+        name.style.backgroundColor = "white"
+        name.style.color = "red"
+        name.style.border = "2px red solid"
+        name.value = "Please provide valid name"
     }
 
-    else {
-
-        textField.style.backgroundColor = "white"; 
-        textField.style.border = "2px solid green"
-        textField.style.color = "black"
-
-        let dice = Math.floor((Math.random()*20)+1); 
-
-        pId.innerHTML = `${dice}`; 
-        diceColor(dice); 
-        hId.innerHTML = `Welcome ${name}`
-        let outcomes = narrativeOutcome(dice); 
-        endResult.innerHTML = `<p>You rolled a ${dice}</p> ${outcomes}`
-
-
+    else{
+        name.style.backgroundColor = "white"
+        name.style.color = "black"
+        name.style.border = "0px white solid"
+        let welcome = document.getElementById("welcome")
+        welcome.innerHTML = `Welcome ${name.value}`
+        return name.value; 
     }
+
+
+    
+
 }
 
-function diceColor(dice) {
-    let diceId = document.getElementById("dice"); 
-    let diceVal = dice; 
-    console.log()
+// generate names 
 
-    switch(diceVal){
-        case 1: 
-            diceId.style.stroke = "red";
-            diceId.style.fill = "red"
-            diceId.style.strokeWidth = "10px"
-            break; 
-        case 20: 
-            diceId.style.stroke = "gold";
-            diceId.style.fill = "gold"
-            diceId.style.strokeWidth = "10px"
-            break; 
-        default: 
-            diceId.style.stroke = "white";
-            diceId.style.fill = "white"
-            diceId.style.strokeWidth = "1px"
+function generateName() {
+    
+    let name = document.getElementById("username")
 
+    // start to generate some names 
+
+    const firstName = ["Shah", "Lara", "Master", "Zelda", "Isaac", "Karlach"]
+    const lastName = ["Rukh-Khan", "Croft", "Chief", "Hyrule", "Clarke", "Cliffgate"]
+
+    let stringName = firstName[Math.floor(Math.random()*6)]+" "+lastName[Math.floor(Math.random()*6)]
+    if(stringName.includes("undefined")){
+        name.value = "Leeroy Jenkins"
+    }
+
+    else{
+        name.value = stringName; 
     }
 
 }
+
+// roll the dice 
+
+function diceRoll(id){
+  let name = document.getElementById("username");
+  if(name.value == "" || name.value == " " || name.value == "Character's Name" || name.value == "Please provide valid name")
+    {
+    alert("Please provide a name before continuing.");
+  } else {
+    let d20 = document.getElementById("dice");
+    let roll = Math.floor(Math.random() * 20 + 1);
+    id.innerHTML = roll;
+
+    switch (roll) {
+      case 1:
+        d20.style.stroke = "red";
+        d20.style.strokeWidth = "10px";
+        break;
+      case 20:
+        d20.style.stroke = "green";
+        d20.style.strokeWidth = "10px";
+        break;
+      default:
+        d20.style.stroke = "white";
+        d20.style.strokeWidth = "1px";
+        break;
+    }
+
+    narrativeOutcome(roll); 
+
+    
+  }
+}
+
+// showcase outcomes 
 
 function narrativeOutcome(dice) {
 
+    let outcome = document.getElementById("outcome")
+
     if(dice == 1) 
     {
-        return "<p>Critical Failure <br/> You get so mad that you walk into the dragon's fire and you ask why do you do this?</p>"
+        outcome.innerHTML =  `You rolled a ${dice} <p>Critical Failure <br/> You get so mad that you walk into the dragon's fire and you ask why do you do this?</p>`
     }
 
     else if(dice >= 2 && dice <= 11)
 
         {
-            return "<p>You realize that you're in Skyrim and you hate it. You really wanted to be in Fallout.</p>"
+            outcome.innerHTML =  `You rolled a ${dice} <p>You realize that you're in Skyrim and you hate it. You really wanted to be in Fallout.</p>`
         }
 
     else if(dice >=12 && dice <= 19 )
     {
-        return "<p>You're in Skyrim, and you've played this game sooo many times it's modded and you're already trying to break it.</p>"
+        outcome.innerHTML =  `You rolled a ${dice} <p>You're in Skyrim, and you've played this game sooo many times it's modded and you're already trying to break it.</p>`
     }
 
     else if(dice == 20)
     {
-        return "<b>Critical Success<b><br/><p>You turn off your PC/Xbox/Switch/PS/Fridge</p>"
+       outcome.innerHTML =  `You rolled a ${dice} <p><b>Critical Success<b><br/><p>You turn off your PC/Xbox/Switch/PS/Fridge</p>`
     }
 }
